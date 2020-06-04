3c7f44be7ad1b18e701fe36959dd867e4d76731b
thpao@example.com <you@example.com>
2020-06-04 09:11:24 +0700

    demo 1

---
+++ xuanthoai_lab654/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="xuanthoai_lab654.MvcApplication" Language="C#" %>
---
+++ xuanthoai_lab654/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(xuanthoai_lab654.Startup))]
+namespace xuanthoai_lab654
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
